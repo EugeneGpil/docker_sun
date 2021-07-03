a4e20fe6a57ba7e3a533e1dcb3778829a535f719
EugeneGpil <gpilligrim@gmail.com>
2021-07-03 14:09:21 +0700

    run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,4 @@
-if command -v ifconfig &> /dev/null
-then
-    export DOCKERHOST=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1)
-else
-    export DOCKERHOST=$(ipconfig | grep -E "IPv4 Address" | awk '{ print $14 }' | cut -f2 -d: | head -n1)
-fi
+export DOCKERHOST=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1)
 cp gsuite-service-account.json php/
 cp gsuite-service-account.json workspace/
 docker-compose up --build
