5827638ee6b2b935b7375f3377d994269acdc818
EugeneGpil <gpilligrim@gmail.com>
2021-09-28 22:59:47 +0700

    1

--- run.sh
+++ run.sh
@@ -1,4 +1,2 @@
-export DOCKERHOST=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1)
 cp gsuite-service-account.json php/
-cp gsuite-service-account.json workspace/
-docker-compose up --build
+docker-compose up --build --remove-orphans
